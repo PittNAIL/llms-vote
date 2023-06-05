b5416140ad1d67a2d518a4e0094e695e8f844eee
David Oniani <onianidavid@gmail.com>
2023-06-05 12:38:17 -0400

    Added rule-based filtering

---
+++ rule_based_filtering.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Author: David Oniani
+# License: MIT
+import json
+
+
+def main() -> None:
+    """Performs rule-based filtering."""
+
+    with open("inverted_index.json", encoding="utf-8") as file:
+        data = {dct["term"]: dct["note_ids"] for dct in json.load(file) if dct["term"] != "disease"}
+
+    total = sum(map(len, data.values()))
+    filtered_data = {}
+    for term, note_ids in data.items():
+        if len(term) < 4 or 0.005 <= len(note_ids) / total:
+            continue
+        filtered_data[term] = note_ids
+    assert sum(map(len, filtered_data.values())) == 25_349
+
+    with open("inverted_index_filtered.json", "w", encoding="utf-8") as file:
+        json.dump(filtered_data, file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
