a79a099bd0226e5c786fe735f80181a3f211f34e
David Oniani <onianidavid@gmail.com>
2023-06-30 15:36:45 -0400

    Sentence embedding generation and benchmarking

--- experiments/sentence_embedding.py
+++ experiments/sentence_embedding.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import train_test_split
 
 from transformers import AutoModel, AutoTokenizer
@@ -21,13 +21,13 @@ def main() -> None:
     model = AutoModel.from_pretrained(MODEL_NAME)
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    # Tokenize the sentence and mention
+    # Tokenize the sentence and mention (or 16, 32)
     df = pd.read_csv("window_size_64.csv")
-    # or 16, 32
 
-    new_data = []
+    emb = []
+    lbl = []
     with torch.no_grad():
-        for index, row in tqdm.tqdm(df.iterrows(), desc="Encoding dataset"):
+        for _, row in tqdm.tqdm(df.iterrows(), desc="Encoding dataset"):
             # This will encode the entire "sentence", being the full context window of 16,32,64
             # words, and return its vector representation.
             inputs = tokenizer(row["window"], return_tensors="pt")
@@ -35,51 +35,39 @@ def main() -> None:
             outputs = model(**inputs)
             # NOTE: sentence embedding is averaged in line with what Dr. Dong does.
             sentence_embedding = torch.mean(outputs.last_hidden_state, dim=1).squeeze(0)
-            new_data.append([sentence_embedding, row["label"]])
 
-    # Convert the new_data list into a pandas DataFrame
-    new_df = pd.DataFrame(new_data, columns=["mention_representation", "label"])
+            emb.append(sentence_embedding)
+            lbl.append(row["label"])
 
-    mention_rep_array = np.vstack(new_df["mention_representation"].to_numpy())
+    # emb = np.array(emb)
+    # lbl = np.array(lbl)
 
-    # print(mention_rep_array)
-
-    X = mention_rep_array
-    y = new_df["label"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1234)
-
-    # set hyperparameter, this is the value set in Dr. Dong's codebase.
-    C = 1
-
-    # max_iter raised b/c 300 (their value) was too low, 500 was too low after I changed it when I
-    # used this script.
-    clf = LogisticRegression(C=C, penalty="l2", max_iter=1000)
-
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_train)
-    training_results = (
-        "training precision: %s" % str(precision_score(y_pred, y_train))
-        + " recall: %s" % str(recall_score(y_pred, y_train))
-        + "F1: %s" % str(f1_score(y_pred, y_train))
+    emb_train, emb_test, lbl_train, lbl_test = train_test_split(
+        emb, lbl, test_size=0.1, random_state=1337
     )
-    print(training_results)
 
-    y_valid_pred = clf.predict(X_test)
-    validation_results = (
-        "testing precision: %s" % str(precision_score(y_valid_pred, y_test))
-        + " recall: %s" % str(recall_score(y_valid_pred, y_test))
-        + "F1: %s" % str(f1_score(y_valid_pred, y_test))
-    )
-    print(validation_results)
+    clf = LogisticRegression(max_iter=1_000)
+    clf.fit(emb_train, lbl_train)
+    pred = clf.predict(emb_test)
 
-    y_pred_whole = clf.predict(X)
-    whole_results = (
-        "whole thing precision: %s" % str(precision_score(y_pred_whole, y))
-        + " recall: %s" % str(recall_score(y_pred_whole, y))
-        + "F1: %s" % str(f1_score(y_pred_whole, y))
+    precision_macro, recall_macro, fscore_macro, _ = precision_recall_fscore_support(
+        pred, lbl_test, average="macro"
     )
-    print(whole_results)
+    precision_weighted, recall_weighted, fscore_weighted, _ = precision_recall_fscore_support(
+        pred, lbl_test, average="weighted"
+    )
+
+    print("Macro Metrics")
+    print("================")
+    print(f"{precision_macro=.2f}")
+    print(f"{recall_macro=.2f}")
+    print(f"{fscore_macro=.2f}")
+
+    print("\nWeighted Metrics")
+    print("================")
+    print(f"{precision_weighted=.2f}")
+    print(f"{recall_weighted=.2f}")
+    print(f"{fscore_weighted=.2f}")
 
 
 if __name__ == "__main__":
