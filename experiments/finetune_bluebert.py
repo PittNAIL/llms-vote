98b56786c25a33618e8e60799478ff9a82575013
Jordan Hilsman <jordanhilsman@gmail.com>
2023-07-04 02:12:00 -0400

    Applied Black, other cleaning of finetuning script.

--- experiments/finetune_bluebert.py
+++ experiments/finetune_bluebert.py
@@ -1,44 +1,39 @@
-from datasets import load_dataset
-import pandas as pd
-import datasets
-
-from datasets import Dataset
+import argparse
 import numpy as np
-import evaluate
+import pandas as pd
 
-from transformers import AutoTokenizer
-from transformers import Trainer, TrainingArguments
-from transformers import AutoModelForSequenceClassification
+import datasets
+from datasets import Dataset
 
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import train_test_split
 
-# dataset = load_dataset("yelp_review_full")
-model_name = "bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import Trainer, TrainingArguments
 
-df = pd.read_csv("window_size_16.csv")
-df_train, df_test = train_test_split(df, test_size=0.1, random_state=1234)
-train_dataset = Dataset.from_dict(df_train)
-test_dataset = Dataset.from_dict(df_test)
-dataset = datasets.DatasetDict({"train": train_dataset, "test": test_dataset})
+MODEL_NAME = "bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+
+def parse_args() -> argparse.Namespace:
+    """Parses command line arguments."""
+
+    parser = argparse.ArgumentParser("csv file for finetuning BlueBERT")
+
+    parser.add_argument(
+        "--data", type=str, help="path to dataset", required=True
+    )  # E: line too long (86 > 79 characters)
+    return parser.parse_args()
 
 
 def tokenize_function(examples):
     return tokenizer(examples["window"], padding="max_length", truncation=True, max_length=128)
 
 
-tokenized_datasets = dataset.map(tokenize_function, batched=True)
-small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
-
-small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
-
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
 
 
-training_args = TrainingArguments(output_dir="test_trainer")
-
-args = TrainingArguments(
+training_args = TrainingArguments(
     per_device_train_batch_size=16,
     per_device_eval_batch_size=64,
     num_train_epochs=3,
@@ -62,20 +57,34 @@ def compute_metrics(eval_pred):
     return {"precision": precision, "recall": recall, "f1": f1}
 
 
-# def compute_metrics(eval_pred):
-#    logits, labels = eval_pred
-#    predictions = np.argmax(logits, axis=-1)
-#    return {'accuracy': (predictions == labels).mean()}
-trainer = Trainer(
-    model=model,
-    args=args,
-    train_dataset=small_train_dataset,
-    eval_dataset=small_eval_dataset,
-    compute_metrics=compute_metrics,
-)
+def main() -> None:
+    """Finetunes BlueBERT from input dataset."""
 
-trainer.train()
+    args = parse_args()
+    df = pd.read_csv(args.data)
+    df_train, df_test = train_test_split(df, test_size=0.1, random_state=1234)
+
+    train_dataset = Dataset.from_dict(df_train)
+    test_dataset = Dataset.from_dict(df_test)
+    dataset = datasets.DatasetDict({"train": train_dataset, "test": test_dataset})
+
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    train_dataset = tokenized_datasets["train"]
+    eval_dataset = tokenized_datasets["test"]
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+    evaluation_result = trainer.evaluate()
+    print(evaluation_result)
 
 
-evaluation_result = trainer.evaluate()
-print(evaluation_result)
+if __name__ == "__main__":
+    main()
